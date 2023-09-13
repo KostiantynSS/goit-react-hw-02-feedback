1c0517923d85246c60c6096620110a5c5ac9ff61
KostiantynSS <storoghenko94@gmail.com>
2023-09-13 09:22:43 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import Control from './Control/Control';
 import Title from './Title/Title';
-import Statistics from './Statistics/Statistics';
-import StatData from './StatData/StatData';
+
 export const App = () => {
   return (
     <div
